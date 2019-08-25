8e0a8bfe2ca79c95ed84d12cbf4b4a78d38947ac
Eilhard <psyvit@gmail.com>
2019-08-25 16:32:55 +0300

    add ability to create new objects

--- src/core/application/components/new_item_editor.vue
+++ src/core/application/components/new_item_editor.vue
@@ -7,14 +7,49 @@
       type="submit">
       New item
     </button>
-    <div v-show="isEditorMode" class="input-group my-2">
-      <input
-        v-model="item"
-        class="form-control list-group-item-action">
-      <div class="input-group-append">
-        <button v-on:click="addNewItem" class="btn btn-outline-secondary" type="button">Save</button>
-        <button v-on:click="isEditorMode = !isEditorMode" class="btn btn-outline-secondary" type="button">Cancel</button>
+    <div v-show="isEditorMode">
+      <div class="input-group my-2">
+        <div class="input-group-prepend">
+          <span class="input-group-text font-weight-bold">Type</span>
+        </div>
+        <select class="custom-select" v-model="itemType">
+          <option value="string">String</option>
+          <option value="number">Number</option>
+          <option value="boolean">Boolean</option>
+          <option value="object">Object</option>
+        </select>
       </div>
+
+      <div v-show="itemType != 'object'" class="input-group my-2">
+        <input
+          v-model="item"
+          class="form-control list-group-item-action">
+        <div class="input-group-append">
+          <button v-on:click="addNewItem" class="btn btn-outline-info" type="button">Save</button>
+          <button v-on:click="isEditorMode = !isEditorMode" class="btn btn-outline-dark" type="button">Cancel</button>
+        </div>
+      </div>
+
+      <form v-show="itemType == 'object'" class="form-inline">
+        <div class="w-100 d-flex flex-wrap justify-content-between">
+          <div class="input-group my-2 item-obj-w">
+            <div class="input-group-prepend">
+              <span class="input-group-text font-weight-bold">Key</span>
+            </div>
+            <input v-model="newObjKey" type="text" class="form-control">
+          </div>
+          <div class="input-group my-2 item-obj-w">
+            <div class="input-group-prepend">
+              <span class="input-group-text font-weight-bold">Value</span>
+            </div>
+            <input v-model="newObjValue" type="text" class="form-control">
+            <div class="input-group-append">
+              <button v-on:click="addNewItem" class="btn btn-outline-info" type="button">Save</button>
+              <button v-on:click="isEditorMode = !isEditorMode" class="btn btn-outline-dark" type="button">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -25,10 +60,17 @@
       return {
         isEditorMode: false,
         item: "",
+        itemType: "string",
+        newObjKey: "",
+        newObjValue: ""
       }
     },
     methods: {
       addNewItem() {
+        if (this.itemType == 'object') {
+          this.item = {};
+          this.item[this.newObjKey] = this.newObjValue;
+        }
         this.$emit('addNewItem', this.item);
         this.isEditorMode = !this.isEditorMode;
         this.item = "";
@@ -38,4 +80,10 @@
 </script>
 
 <style scoped>
+  .item-obj-w {
+    width: 49.5%;
+    @media (max-width: 650px) {
+      width: 100%;
+    }
+  }
 </style>
